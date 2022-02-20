3f9c527db0a38a0bb3116a2043616ac1a75040b7
Thiago Costa <tjthiagocosta@gmail.com>
2022-02-19 23:36:51 -0300

    added https to the API request

--- weather-app/app.js
+++ weather-app/app.js
@@ -43,7 +43,7 @@
 
   // Get weather from API provider
   function getWeather(latitude, longitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     fetch(api)
       .then(function (response) {
